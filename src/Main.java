c122667defbd73f9691a030ffcc4a7aca963891c
wellingtonfcdev <wellingtonfc@hotmail.com>
2024-06-15 09:36:56 -0300

    Igualdade entre objetos

--- src/Main.java
+++ src/Main.java
@@ -16,11 +16,18 @@ public class Main {
         // casa3.nome = "Minha casa";
         // System.out.println(casa3.nome);
 
-        Double soma = Matematica.soma(90, 2);
-        Double somaNaoEstatica = new Matematica().somaNaoEstatica(90, 2);
-        System.out.println(soma);
-        System.out.println(somaNaoEstatica);
+        // Double soma = Matematica.soma(90, 2);
+        // Double somaNaoEstatica = new Matematica().somaNaoEstatica(90, 2);
+        // System.out.println(soma);
+        // System.out.println(somaNaoEstatica);
         
+      Usuario u1 = new Usuario();
+      u1.username = "u1";
+      Usuario u2 = new Usuario();
+      u2.username = "u1";
 
+      System.out.println(u1.equals(u2));
+    //   System.out.println(u1);
+    //   System.out.println(u2);
     }
 }
--- src/Usuario.java
+++ src/Usuario.java
@@ -3,4 +3,34 @@ public class Usuario {
     String username;
     String email;
     String password;
+
+    public String toString(){
+        return "{username: " + username + ", email: " + email + ", password: " + password + "}";
+    }
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((username == null) ? 0 : username.hashCode());
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        Usuario other = (Usuario) obj;
+        if (username == null) {
+            if (other.username != null)
+                return false;
+        } else if (!username.equals(other.username))
+            return false;
+        return true;
+    }
+    
 }
