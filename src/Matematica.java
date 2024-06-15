c80a8f90f979193292388ff4f101645cde372b0b
wellingtonfcdev <wellingtonfc@hotmail.com>
2024-06-15 08:44:18 -0300

    Modificador final

--- src/Matematica.java
+++ src/Matematica.java
@@ -1,19 +1,19 @@
 public class Matematica {
 
-    public static Double limite = 100.0;
+    public static final Double LIMITE_OPERACA_DOUBLE = 100.0; //SNAKE_CASE
 
     
     public static Double soma(double n1, double n2){
         double soma = n1 + n2;
-        if (soma > limite)
-            return limite;
+        if (soma > LIMITE_OPERACA_DOUBLE)
+            return LIMITE_OPERACA_DOUBLE;
         else return soma;
     }
 
     public Double somaNaoEstatica(double n1, double n2){
         double soma = n1 + n2;
-        if(soma > limite)
-            return limite;
+        if(soma > LIMITE_OPERACA_DOUBLE)
+            return LIMITE_OPERACA_DOUBLE;
         else return soma;
     }
 }
