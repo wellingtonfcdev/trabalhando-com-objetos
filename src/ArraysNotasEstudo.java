88beac989efd363f3af2d557b8c6515f75f05f2e
wellingtonfcdev <wellingtonfc@hotmail.com>
2024-06-15 08:54:25 -0300

    Arrays primitivos

---
+++ src/ArraysNotasEstudo.java
@@ -0,0 +1,16 @@
+import java.util.Arrays;
+
+public class ArraysNotasEstudo {
+    
+    //Arrays de primitivos
+    public static void main(String[] args) {
+        int[] idades = {5,10,18,30,34};
+
+        System.out.println(Arrays.toString (idades));
+
+    }
+    
+
+
+
+}
